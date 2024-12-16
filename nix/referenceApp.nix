7f5acd746cb7ce5ebc8552da7aed85909f856e4b
Thomas Frank <thomas.frank@esrlabs.com>
2024-12-16 16:31:54 +0100

    Add basic nix infrastructure

---
+++ nix/referenceApp.nix
@@ -0,0 +1,21 @@
+{ stdenv
+, cmake
+}:
+stdenv.mkDerivation {
+  name = "referenceApp";
+  src = ../.;
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  configurePhase = ''
+    cmake -B cmake-build-posix -S executables/referenceApp
+  '';
+  buildPhase = ''
+    cmake --build cmake-build-posix --target app.referenceApp -j $NIX_BUILD_CORES
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp cmake-build-posix/application/app.referenceApp.elf $out/bin/referenceApp
+  '';
+}
