53eb37302b1775afba487064c1b59322a99eca44
Thomas Frank <thomas.frank@esrlabs.com>
2024-12-16 18:03:35 +0100

    Try to run pytest

--- nix/IntegrationTests.nix
+++ nix/IntegrationTests.nix
@@ -33,13 +33,43 @@ pkgs.nixosTest {
     environment.systemPackages = [
       self.packages."${system}".referenceApp
       pkgs.can-utils
+      (pkgs.python3.withPackages (p: [
+        p.can-isotp# = 2.0.6
+        p.exceptiongroup# = 1.2.2
+        p.iniconfig# = 2.0.0
+        p.msgpack# = 1.0.8
+        p.packaging# = 24.1
+        p.pluggy# = 1.5.0
+        p.pyserial# = 3.5
+        p.pytest# = 8.3.3
+        p.python-can# = 4.4.2
+        p.tomli# = 2.0.1
+        p.typing-extensions# = 4.12.2
+        # p.udsoncan# = 1.23.1
+        (pkgs.python3Packages.buildPythonPackage rec {
+          pname = "udsoncan";
+          version = "1.23.1";
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-XlfzNk48aQpJ0Xsvdmj6VsNkr37J2PUT71gkFzMcpIA=";
+          };
+        })
+        p.wrapt# = 1.16.0
+
+      ]))
     ];
+
+    environment.variables = {
+      OPENBSW_SRC = ../.;
+    };
 
   };
 
   testScript = { nodes, ... }: ''
     machine.wait_for_unit("default.target")
     machine.succeed("ifconfig vcan0")
+    machine.succeed("ls $OPENBSW_SRC | grep README.md")
+    machine.succeed("cd $OPENBSW_SRC/test/pyTest && pytest --target=posix")
   '';
 
 }
