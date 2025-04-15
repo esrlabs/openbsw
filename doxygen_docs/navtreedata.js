a02903f4d53f0ba19a495213224a858817b0bd08
marcmo <marcmo@users.noreply.github.com>
2025-04-15 08:34:08 +0000

    deploy: ab0d433c4b43f8de9634ae7c92efcee5e9154c35

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Todo List", "dd/da0/todo.html", null ],
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d77/classdocan_1_1DoCanMessageTransmitProtocolHandler.html#a65d478b068d79a1d77b693b095b61761",
+"d2/d66/classio_1_1MemoryQueueReader.html#add02190284ccc798b4a80686cf358bcb",
+"d3/d99/namespaceuds.html#a560875185cb8ddb373aaef7cd5ecb6c0",
+"d4/df1/classuds_1_1PositiveResponse.html#a7ade620e81ebb535aed4ab591ddd4a6f",
+"d6/d38/structlifecycle_1_1LifecycleManager_1_1ComponentInfo.html#a4affaa35ba5c1ae650c077df89217334",
+"d7/da2/classuds_1_1NestedDiagRequest.html#ae0e02165cdcb73f0049567bbef114b36",
+"d8/dfd/ordered__map_8h.html#aab793f0ef8a8c068c3059244dcae7086",
+"da/d3c/classutil_1_1stream_1_1StdoutStream.html",
+"db/d64/classdocan_1_1IDoCanDataFrameTransmitter.html#a797b389bb12d52ebdaf84dbe2f12890d",
+"dc/d35/structio_1_1internal_1_1max__element__size.html",
+"dd/d42/AbstractStaticBitFieldFilter_8h.html",
+"de/dc0/classutil_1_1memory_1_1BuddyMemoryManager.html#a5cc8b1d5caa95df184d97490164aa3c4",
+"df/dfb/EnableRapidPowerShutdown_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
