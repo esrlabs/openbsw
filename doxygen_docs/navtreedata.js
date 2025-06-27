49660415d1a6a27796bbee909ebcc3e41b5647b2
marcmo <marcmo@users.noreply.github.com>
2025-06-27 13:52:20 +0000

    deploy: e6acc47cc343e4e15990989a45a9e96d523a5b0f

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,90 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"",
+"d0/da1/classestd_1_1slice_3_01T_00_010_01_4.html#a0dd8fb0743a5324483d7caecd3d09cff",
+"d1/d69/classuds_1_1DiagnosticSessionControl.html#a6b1dd814cbd2d723baaaa34aed9c703d",
+"d2/d05/classcan_1_1AbstractStaticBitFieldFilter.html#a578b0971f68743508bec21178b532e3c",
+"d2/dbd/classestd_1_1declare_1_1ordered__vector.html#a740bb72fd9f8c2baa815a658707f9b68",
+"d3/d55/classestd_1_1string.html#afa4004bd7ff0d0cc6aa035cfe6a91495",
+"d4/d41/classuds_1_1declare_1_1AsyncDiagHelper.html#a2d9758f507b32de4ede66c1f59b3b57b",
+"d4/ddc/classuds_1_1MultipleReadDataByIdentifier.html#a99794c419a7c1189d7ffc3a3be967191",
+"d5/dee/classestd_1_1vec.html#a02fa6898fe3a88a019b55b45e1c65265",
+"d6/dbd/classutil_1_1format_1_1PrintfFormatter.html#add9d22aa4e753c90644e2b1394b66246",
+"d7/da2/classuds_1_1NestedDiagRequest.html#aa1838520d2a418db068952a44ca2d0b1",
+"d8/d42/classestd_1_1declare_1_1object__pool.html#a7f75d3a1268960ad67b6915c49c9c29f",
+"d8/df0/classestd_1_1typed__mem.html",
+"d9/dcd/structestd_1_1internal_1_1__limited__storage.html",
+"da/d64/classcan_1_1IFilteredCANFrameSentListener.html#a81efdabe2de2c0f73980498599eb35c9",
+"da/de5/classbios_1_1AlternativeDigitalInput.html",
+"db/d99/classuds_1_1ApplicationDefaultSession.html#ac5c092bcb72a55dbd1c684a3a38372a4",
+"dc/d06/classestd_1_1internal_1_1by__ref__wrapper.html",
+"dc/dbf/JoinReaderExample_8cpp.html#af854f4dc65fabf76cf553dd7ec56703c",
+"dd/d35/classestd_1_1forward__list.html#a29d261dcbdffd3971cc97af596ce639a",
+"dd/da4/Logger_8h_source.html",
+"de/dc0/classutil_1_1memory_1_1BuddyMemoryManager.html#a2897bbb6359c23f056b9c374c8973d3d",
+"df/d76/classestd_1_1ordered__vector.html#ac8d1ece0e1e83b9dc356a1a63f32f606",
+"functions_g.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
