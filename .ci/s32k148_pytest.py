f2d7a4114420e9492aa8ddf73d51d71df8edad57
Shamitha Shashidhara <shamitha.shashidhara@accenture.com>
2025-11-11 11:10:10 +0530

    Add Python script to execute Nightly build
    - Introduced Python script to run pytest for S32K148 platform
    - Executes build and tests for both ThreadX and FreeRTOS
    
    Change-Id: I2b46c74f9c6b0f163fccada66894b90ac6a95470

---
+++ .ci/s32k148_pytest.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+import sys
+
+def run_cmd(cmd, cwd=None):
+    print(f"\n>>> Running: {cmd}", flush=True)
+    subprocess.run(cmd, shell=True, check=True, cwd=cwd)
+
+def main():
+    target = "s32k148"
+    toolchain = "gcc"
+    cxxstd = "14"
+    matrix = [
+        {"app": "freertos", "preset": "s32k148-freertos-gcc"},
+        {"app": "threadx",  "preset": "s32k148-threadx-gcc"},
+    ]
+    for item in matrix:
+        preset = item["preset"]
+        run_cmd(f'python3 .ci/build.py --preset "{preset}" --platform "arm" --cxxid "{toolchain}" --cxxstd "{cxxstd}"')
+
+    os.chdir("test/pyTest")
+
+    for item in matrix:
+        app = item["app"]
+        run_cmd(f'pytest -s --target={target} --no-restart --app={app}')
+
+if __name__ == "__main__":
+    try:
+        main()
+    except subprocess.CalledProcessError as e:
+        print(f"\nERROR: command failed with exit code {e.returncode}")
+        sys.exit(e.returncode)
+
