8b9cb2e6e7356c5f54e4dbe9ef2e3e8ee8d056e7
neeraja.madhu <neeraja.madhu@accenture.com>
2025-11-19 11:44:06 +0100

    Skip stats related test for posix-threadx as stats commands are not currently supported
    Change-Id: Ia53674da66469add21d4d3215f071043b7e8c408

--- test/pyTest/console/test_stats_cpu.py
+++ test/pyTest/console/test_stats_cpu.py
@@ -1,27 +1,26 @@
+import pytest
 from time import sleep
 
 
+@pytest.mark.skip_if("target==['posix'] and app=='threadx'")
 def test_console_stats_cpu(target_session):
-
     capserial = target_session.capserial()
     assert capserial.wait_for_boot_complete()
 
     # stats commands need a second after boot
     # and seems to need some time between successive calls to it
     sleep(1)
-
     capserial.clear()
-
     capserial.send_command(b"stats cpu\n")
     (success, _, _) = capserial.read_until(
-        [b"ok",
-         b"Received console command \"stats cpu\"",
-         b"Console command succeeded"],
+        [b"ok", b'Received console command "stats cpu"', b"Console command succeeded"],
         timeout=2.0,
-        match_all_expected=False)
+        match_all_expected=False,
+    )
     assert success
 
 
+@pytest.mark.skip_if("target==['posix'] and app=='threadx'")
 def test_console_cpu_health(target_session):
 
     capserial = target_session.capserial()
@@ -30,14 +29,12 @@ def test_console_cpu_health(target_session):
     # stats commands need a second after boot
     # and seems to need some time between successive calls to it
     sleep(1)
-
     capserial.clear()
-
     capserial.send_command(b"stats cpu\n")
     (success, lines, _) = capserial.read_until(b" idle ", timeout=2.0)
     assert success
 
     parts = lines[-1].split()
-    assert (parts[0] == b"idle")
+    assert parts[0] == b"idle"
     percent_idle = float(parts[1])
     assert percent_idle > 10, f"Idle time {percent_idle}% is not > 10%"
