83211219af172f40d3efa594b9dc0793a479ba6d
marcmo <marcmo@users.noreply.github.com>
2025-11-28 11:07:40 +0000

    deploy: 79882fe9e59e9c233ce1051ffc0d7586006d061e

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,82 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d47/classudp_1_1LwipDatagramSocket.html#a1a09088f5538aac64654be5016b682dd",
+"d1/de8/WriteDataByIdentifier_8h_source.html",
+"d2/dba/classtcp_1_1LwipSocket.html#addcbf9df000f5ee54f546c9eca3578a8",
+"d3/d59/DoCanMessageTransmitProtocolHandler_8h.html#a9bfd33979dc74fcadf1f46db508121daabdade8b55322f75f1c7f5d6d660a2cf1",
+"d4/d2f/classutil_1_1spsc_1_1Queue_1_1Sender_1_1Write.html#a8945e4400894a92f75d6b1b875ab27ca",
+"d5/d58/namespacetcp.html",
+"d6/d4f/classbios_1_1Adc.html#a588805708a69e2306f774e090bb82d92",
+"d7/d69/typedefs_8h_source.html",
+"d8/d1e/classuds_1_1IResumableResetDriverPersistence.html#aec4ec283c2bcf77187bf51e79f3aa544",
+"d8/dfc/ReadDataByIdentifier_8h_source.html",
+"d9/dbc/classutil_1_1stream_1_1TaggedSharedOutputStream.html#a13bb9da7e599b42b9590e6a4f77c320b",
+"da/d8c/classtransport_1_1TransportMessageSendJob.html#a18fdb07d7ce6329e619304c6496f0bd1",
+"db/d1c/classtcp_1_1AbstractServerSocket.html#a6d03559f4909d0d44974e7eefaf28c80",
+"db/ddc/classeepromemulation_1_1IEepromEmulationDriver.html#a81f48582b7d00306fa3da3574b6bac0fa3058c9706adb0968993f3997a7e43081",
+"dc/d7e/classenetphy_1_1Tja1101.html#aa27d4f34714ec541e869aea29bce1514a0d823c07b6e0f33ea303b8aebf8d28d0",
+"dd/d00/DataIdentifierJob_8h_source.html",
+"dd/dd4/classutil_1_1stream_1_1StdinStream.html",
+"de/de9/TpRouterLogger_8h_source.html",
+"df/da7/classlogger_1_1SharedStreamEntryOutput.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
